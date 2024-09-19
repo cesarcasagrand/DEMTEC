62d2fc43bedadf8d6bd5cb2487032500d25cc1f4
cesarcasagrand <ccasagrandelopes@gmail.com>
2024-09-19 16:42:41 -0300

    Teste

--- DEMTEC-CLIENTE/src/main.jsx
+++ DEMTEC-CLIENTE/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+a = 2
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
